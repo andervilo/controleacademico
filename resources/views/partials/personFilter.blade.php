9d8cb0e4545d4d14cad9da61c5f99b5532e18c0b
andervilo <andervilo@hotmail.com>
2018-12-11 14:03:40 -0300

    .

--- resources/views/partials/personFilter.blade.php
+++ resources/views/partials/personFilter.blade.php
@@ -1,13 +1,16 @@
-<form method="get" style="display:inline-block;width: 380px;" action="">
+<div class="col-md-11">
+        <form method="get" style="display:inline-block;" action="">
 
-    <div class="input-group">
-        <input type="text" name="q" value="{!!$q!!}" class="form-control input pull-right" placeholder="Buscar por Nome, Id Funcional, RG, CPF ou E-mail">
+                <div class="input-group">
+                    <input type="text" name="q" value="{!!$q!!}" class="form-control input pull-right" placeholder="Buscar por Nome, Id Funcional, RG, CPF ou E-mail">
 
-        <div class="input-group-btn">
-            <button title="Aplicar Filtro" type="submit" class="btn  btn-default"><i class="fa fa-search"></i></button>
-        </div>
-    </div>
-</form>
+                    <div class="input-group-btn">
+                        <button title="Aplicar Filtro" type="submit" class="btn  btn-default"><i class="fa fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+</div>
+
 
 <form method="get" style="display:inline-block;width: 30px;" action="">
 
