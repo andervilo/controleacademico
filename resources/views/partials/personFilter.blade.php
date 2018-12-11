1ce3a44daff6777b87cb52233cea67b0f53a043d
andervilo <andervilo@hotmail.com>
2018-12-11 14:08:19 -0300

    .

--- resources/views/partials/personFilter.blade.php
+++ resources/views/partials/personFilter.blade.php
@@ -1,22 +1,26 @@
-<div class="col-md-11">
+<div class="col-md-12">
+    <div class="col-md-11">
         <form method="get" style="display:inline-block;" action="">
+            <div class="input-group">
+                <input type="text" name="q" value="{!!$q!!}" class="form-control input pull-right" placeholder="Buscar por Nome, Id Funcional, RG, CPF ou E-mail">
 
-                <div class="input-group">
-                    <input type="text" name="q" value="{!!$q!!}" class="form-control input pull-right" placeholder="Buscar por Nome, Id Funcional, RG, CPF ou E-mail">
-
-                    <div class="input-group-btn">
-                        <button title="Aplicar Filtro" type="submit" class="btn  btn-default"><i class="fa fa-search"></i></button>
-                    </div>
+                <div class="input-group-btn">
+                    <button title="Aplicar Filtro" type="submit" class="btn  btn-default"><i class="fa fa-search"></i></button>
                 </div>
-            </form>
+            </div>
+        </form>
+    </div>
+
+    <div class="col-md-1">
+        <form method="get" style="display:inline-block;width: 30px;" action="">
+            <div class="input-group">
+                <div class="input-group-btn">
+                    <button title="Zerar Filtro" type="submit" class="btn  btn-danger"><i class="fa fa-eraser"></i></button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 
 
-<form method="get" style="display:inline-block;width: 30px;" action="">
 
-    <div class="input-group">
-        <div class="input-group-btn">
-            <button title="Zerar Filtro" type="submit" class="btn  btn-danger"><i class="fa fa-eraser"></i></button>
-        </div>
-    </div>
-</form>&nbsp;&nbsp;
